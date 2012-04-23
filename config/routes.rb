96776faaffc059f0d28f3712244a1bd8be744320
Kjellski <otto.kjell@gmail.com>
2012-04-23 22:04:58 +0200

    Finished static pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Railstutorial::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
+  get "static_pages/about"
 
   resources :microposts
-
   resources :users
 
   # The priority is based upon order of creation:
